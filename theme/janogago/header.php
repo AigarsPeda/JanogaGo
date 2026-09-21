66fff654f7a3b2fd9aa2fdd595ae627c6e6cd25b
Aigars <aigarspeda@gmail.com>
2026-09-21 13:57:37 +0300

    Add GO favicon

--- theme/janogago/header.php
+++ theme/janogago/header.php
@@ -1,6 +1,12 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head><meta charset="<?php bloginfo( 'charset' ); ?>"><meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"><?php wp_head(); ?></head>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+	<link rel="icon" href="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/favicon-go.svg' ); ?>" type="image/svg+xml" sizes="any">
+	<link rel="apple-touch-icon" href="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/images/favicon-go.png' ); ?>">
+	<?php wp_head(); ?>
+</head>
 <body <?php body_class(); ?>><?php wp_body_open(); ?>
 <header class="site-header">
 	<div class="site-brand">
